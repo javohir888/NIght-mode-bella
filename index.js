82eb1e5e148a4380d8da5433f20a454c8e5c4df2
javohir888 <javohirj686@gmail.com>
2021-05-07 18:11:12 +0500

    hi

--- index.js
+++ index.js
@@ -1,17 +1,76 @@
-function Dog (name, age, zoti) {
-    this.name = name;
-    this.age = age;
-    this.zoti = zoti;
-}
+let circle = document.querySelector(".circle");
+let knock = document.querySelector(".knock");
+let insta = document.querySelector(".insta");
 
-let artos = new Dog("Artos", 4, "Alabay");
-let charli = new Dog("Charli", 3, "tozi");
 
-console.log(charli.age);
 
-Dog.prototype.bark = function() {
-    console.log("WOW");
-}
+circle.addEventListener("click", ()=> {
+    circle.classList.toggle("go-toggle");
+    knock.classList.toggle("go-knock");
+    insta.classList.toggle("go-insta");
+});
 
-console.log(artos.bark());
-console.log(charli.bark());
+insta.addEventListener("click", ()=> {
+    insta.classList.toggle("go-insta");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Dog (name, age, zoti) {
+//     this.name = name;
+//     this.age = age;
+//     this.zoti = zoti;
+// }
+
+// let artos = new Dog("Artos", 4, "Alabay");
+// let charli = new Dog("Charli", 3, "tozi");
+
+// console.log(charli.age);
+
+// Dog.prototype.bark = function() {
+//     console.log("WOW");
+// }
+
+// console.log(artos.bark());
+// console.log(charli.bark());
